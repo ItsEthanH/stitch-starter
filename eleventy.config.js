4cd1891af6aa9f1169e268c8449745d031e1d502
ItsEthanH <ethanhawes@outlook.com>
2023-02-24 17:26:07 +0000

    markup, basic styles set up

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,8 @@
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.ignores.add('./src/_kit/**/*');
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/images');
