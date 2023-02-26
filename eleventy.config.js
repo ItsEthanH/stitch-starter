a5fa28443304356bf5d91d55980dcb03efb919b0
ItsEthanH <ethanhawes@outlook.com>
2023-02-26 12:08:39 +0000

    fixed styling

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,8 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/images'); // DELETE IF USING feature/image-optimisation
 
-  eleventyConfig.addWatchTarget('./src/assets/*');
-  eleventyConfig.addWatchTarget('./src/sass/*');
+  eleventyConfig.addWatchTarget('./src/assets/*/**');
+  eleventyConfig.addWatchTarget('*/**.scss');
 
   return {
     // default is to use nunjucks to render everything - feel free to change this to whatever you want
