84fc68ed420da36bdd62c46fd5c60b0cd0f2c3c0
ItsEthanH <ethanhawes@outlook.com>
2023-04-02 13:20:08 +0100

    removing scrolled header mechanic

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -13,17 +13,3 @@ function backgroundClose(event) {
 // listeners
 document.getElementsByTagName('nav')[0].addEventListener('click', backgroundClose);
 document.getElementById('hamburger').addEventListener('click', toggleMenu);
-
-// changes the nav background on when a user scrolls > 0px
-// this one is optional - feel free to delete this and the corrosponding .styled classes
-function changeNavBackground() {
-  const header = document.getElementsByTagName('header')[0];
-
-  if (window.scrollY !== 0) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-}
-
-document.addEventListener('scroll', changeNavBackground);
